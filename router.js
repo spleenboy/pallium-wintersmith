a1730479d9c436f09458a1787e52f6f14cbcdd91
Barrett Alexander <barrett@clockwork.net>
2015-06-19 09:25:43 -0500

    Updated router code to work with new factory

--- router.js
+++ router.js
@@ -2,8 +2,9 @@ var Controller = plugin('controllers/controller');
 var WintersmithController = require('./controller');
 
 module.exports = function(event) {
-    var router = new event.express.Router();
-    router.get('/preview', Controller.handle('preview', WintersmithController));
-    router.get('/publish', Controller.handle('publish', WintersmithController));
+    var router  = new event.express.Router();
+    var factory = new Controller.Factory(WintersmithController, event.app);
+    router.get('/preview', factory.handle('preview'));
+    router.get('/publish', factory.handle('publish'));
     return router;
 };
